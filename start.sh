81d7a51010b0ea38a066728f5f09e9b14bfefcaa
iwhiwh <131843374+iwhiwh@users.noreply.github.com>
2023-10-19 22:27:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/iwhiwh/kuutuuBot /kuutuuBot
+  git clone https://github.com/iwhiwh/Sunil-Ka-Bot /Sunil-Ka-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /kuutuuBot
+  git clone $UPSTREAM_REPO /Sunil-Ka-Bot
 fi
-cd /kuutuuBot
+cd /Sunil-Ka-Bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot....😁😁😁"
 python3 bot.py
